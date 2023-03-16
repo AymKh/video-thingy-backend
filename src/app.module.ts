80a82d10410ff0c071c79e57258bd176293a8145
aymen.khedhriya.ines@gmail.com <aymen4real@gmail.com>
2023-03-16 13:38:16 +0100

    videos modle

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { VideosModule } from './videos/videos.module';
 
 @Module({
   imports: [TypeOrmModule.forRoot({
@@ -14,7 +15,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
     database: 'video-thingy',
     entities: ["dist/**/*.js"],
     synchronize: true,
-  })],
+  }), VideosModule],
   controllers: [AppController],
   providers: [AppService],
 })
