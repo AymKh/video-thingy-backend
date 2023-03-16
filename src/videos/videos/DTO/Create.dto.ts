244556307d125bc2634de306f6c0efbef89cae51
aymen.khedhriya.ines@gmail.com <aymen4real@gmail.com>
2023-03-16 13:49:37 +0100

    CRUD routing + dtos

---
+++ src/videos/videos/DTO/Create.dto.ts
@@ -0,0 +1,10 @@
+/* eslint-disable prettier/prettier */
+export class CreateDTO {
+    title: string;
+    video_link: string;
+    video_genre: string;
+    video_visibility: boolean;
+    video_views: number;
+    video_createdOn: Date;
+    video_modifiedOn: Date;
+}
--- src/videos/videos/videos.controller.ts
+++ src/videos/videos/videos.controller.ts
@@ -1,5 +1,45 @@
 /* eslint-disable prettier/prettier */
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Post, Put } from '@nestjs/common';
+// DTOS
+import { CreateDTO } from './DTO/Create.dto';
+import { UpdateDTO } from './DTO/Update.dto';
 
 @Controller('videos')
-export class VideosController { }
+export class VideosController {
+
+    // GET ALL VIDEOS
+    @Get('/')
+    getAllVideos() {
+        console.log('videos...');
+        return [];
+    }
+
+    // GET ONE VIDEO
+    @Get(':id')
+    getOneVideo(@Param('id', ParseIntPipe) id: number) {
+        console.log('video...');
+        return id;
+    }
+
+    // CREATE ONE VIDEO
+    @Post('/')
+    createOneVideo(@Body() payload: CreateDTO) {
+        return null;
+    }
+
+    // UPDATE ONE VIDEO
+    @Put(':id')
+    async updateOneVideo(
+        @Param('id', ParseIntPipe) id: number,
+        @Body() payload: UpdateDTO
+    ) {
+        return null;
+    }
+
+    // DELETE ONE VIDEO
+    @Delete(':id')
+    async deleteOneVideo(@Param('id', ParseIntPipe) id: number) {
+        return null;
+    }
+
+}
