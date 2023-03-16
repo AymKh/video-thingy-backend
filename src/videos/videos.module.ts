59f2d311462a759db42a2b259d9eadc61a9d7a74
aymen.khedhriya.ines@gmail.com <aymen4real@gmail.com>
2023-03-16 16:00:15 +0100

    video service

--- src/videos/videos.module.ts
+++ src/videos/videos.module.ts
@@ -1,8 +1,10 @@
 /* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { VideosController } from './videos/videos.controller';
+import { VideosService } from './videos/videos.service';
 
 @Module({
-  controllers: [VideosController]
+  controllers: [VideosController],
+  providers: [VideosService]
 })
 export class VideosModule { }
