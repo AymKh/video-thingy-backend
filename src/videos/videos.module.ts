d93fb2e3c9f35195e37507763afc3e93b41a68a5
aymen.khedhriya.ines@gmail.com <aymen4real@gmail.com>
2023-03-16 16:07:09 +0100

    service hookup

--- src/videos/videos.module.ts
+++ src/videos/videos.module.ts
@@ -1,9 +1,13 @@
 /* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { VideosController } from './videos/videos.controller';
 import { VideosService } from './videos/videos.service';
+import { Video } from 'src/typeorm/entities/Video';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Video])],
   controllers: [VideosController],
   providers: [VideosService]
 })
