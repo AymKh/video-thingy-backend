3105b50863e0656d78fa94ea0a06997b9565e614
aymen.khedhriya.ines@gmail.com <aymen4real@gmail.com>
2023-03-22 14:32:01 +0100

    signin

--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -1,12 +1,18 @@
 /* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { JwtModule } from '@nestjs/jwt';
 import { AuthController } from './auth.controller';
 import { AuthService } from 'src/app/services/auth/auth.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/app/models/User';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    JwtModule.register({
+      secret: 'lol-secret-here'
+    })
+  ],
   controllers: [AuthController],
   providers: [AuthService]
 })
--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/no-empty-function */
 /* eslint-disable prettier/prettier */
 import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from 'src/app/models/User';
 import { AuthDTO } from 'src/app/modules/auth/DTO/Auth.dto';
@@ -9,18 +10,29 @@ import { Repository } from 'typeorm';
 @Injectable()
 export class AuthService {
 
-    constructor(@InjectRepository(User) private userRepo: Repository<User>) { }
+    constructor(
+        @InjectRepository(User) private userRepo: Repository<User>,
+        private jwtService: JwtService
+    ) { }
 
     async signinLocal(payload: AuthDTO) {
         const data = await this.userRepo.findOneBy({ username: payload.username });
         if (!data) throw new UnauthorizedException('Credentials provided are incorrect.');
         if (data.password !== payload.password) throw new UnauthorizedException('Credentials provided are incorrect.');
-        return data;
+        return this.signZeUser(data.user_id, data.username, 'user');
     }
 
 
 
     signupLocal(payload: AuthDTO) {
         return null;
+    }
+
+    signZeUser(id: number, email: string, type: string) {
+        return this.jwtService.sign({
+            id,
+            email,
+            claim: type
+        })
     }
 }
