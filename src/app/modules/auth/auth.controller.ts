9744f25825e57f44a250ec2ca06716227ac8804a
aymen.khedhriya.ines@gmail.com <aymen4real@gmail.com>
2023-04-02 15:50:27 +0100

    logout route

--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-empty-function */
 /* eslint-disable prettier/prettier */
-import { Body, Controller, Post, Request, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
 import { LocalAuthGuard } from '../../guards/local.guard';
 import { UsersService } from 'src/app/services/users/users.service';
 import { CreateUsertDTO } from '../users/DTO/CreateUser.dto';
@@ -13,6 +13,12 @@ export class AuthController {
     @Post('signin')
     signinLocal(@Request() req) {
         return req.user;
+    }
+
+    @Get('logout')
+    signoffLocal(@Request() req) {
+        req.session.destroy();
+        return { msg: 'user logged out' }
     }
 
     @Post('signup')
